482161b418aa0a1ad2906fff6801caddccc43893
Dima <dimakurinnoy8@gmail.com>
2025-10-25 12:10:52 +0200

    new title

--- src/components/ClaimWindow/ClaimWindow.tsx
+++ src/components/ClaimWindow/ClaimWindow.tsx
@@ -4,7 +4,7 @@ import style from "./ClaimWindow.module.scss";
 export const ClaimWindow = () => {
   return <div className={style.window}>
 
-<h2 className={style.title_1}>Claim Tokens</h2>
+<h2 className={style.title_1}>Presale has finished</h2>
 
 
 <ClaimBlock/>
